3c7be9c2ec432f13ef5c11122e9913783e865e32
seguteja <sivateja@skystop.tech>
2021-11-12 14:53:04 +0530

    new

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express=require('express');
 const app=new express();
 
 app.get('/',async(req,res)=>{
-    res.send("Successfully deployed")
+    res.send("Successfully deployed for first time")
 });
 
 app.listen(3333,()=>{
