b549d68a5ca0ca4b4f3df5cbe811416bb2fc7844
seguteja <84762434+seguteja@users.noreply.github.com>
2021-11-12 15:08:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express=require('express');
 const app=new express();
 
 app.get('/',async(req,res)=>{
-    res.send("Successfully deployed for first time")
+    res.send("Successfully deployed for second time")
 });
 
 app.listen(3333,()=>{
